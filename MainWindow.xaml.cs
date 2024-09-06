de901d69a070871a51be5883c4287fd1dad3868e
Györfi Marcell <44555742+gyorfimarcell@users.noreply.github.com>
2024-09-06 09:51:42 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace WPF_Brickstore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Part> parts = [];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            dgParts.ItemsSource = parts;
+        }
+
+        private void FilterParts()
+        {
+            if (parts.Count > 0)
+            {
+                dgParts.ItemsSource = parts.Where(x =>
+                    (tbName.Text == "" || x.Name.Contains(tbName.Text, StringComparison.CurrentCultureIgnoreCase))
+                    && (tbId.Text == "" || x.Id.Contains(tbId.Text, StringComparison.CurrentCultureIgnoreCase))
+                    && (cbCategory.SelectedIndex == 0 || x.Category == cbCategory.SelectedItem.ToString())
+                );
+            }
+        }
+
+        private void ClearFilters()
+        {
+            tbName.Text = "";
+            tbId.Text = "";
+            cbCategory.SelectedIndex = 0;
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new();
+            ofd.Filter = "BrickStore fájl (.bsx)|*.bsx";
+
+            if (ofd.ShowDialog() == true)
+            {
+                parts.Clear();
+
+                XDocument xaml = XDocument.Load(ofd.FileName);
+                foreach (var item in xaml.Descendants("Item"))
+                {
+                    parts.Add(new Part(
+                        item.Element("ItemID").Value,
+                        item.Element("ItemName").Value,
+                        item.Element("CategoryName").Value,
+                        item.Element("ColorName").Value,
+                        Convert.ToInt32(item.Element("Qty").Value)
+                    ));
+                }
+
+                lblFile.Content = $"Betöltött fájl: {ofd.FileName} ({parts.Count} elem)";
+
+                cbCategory.ItemsSource = parts.Select(x => x.Category).Distinct().Order().Prepend("Összes kategória");
+                ClearFilters();
+            }
+        }
+
+        private void tbName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            FilterParts();
+        }
+
+        private void tbId_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            FilterParts();
+        }
+
+        private void cbCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            FilterParts();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            ClearFilters();
+        }
+    }
+}
---
+++ Part.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Brickstore
+{
+    internal class Part
+    {
+        string id, name, category, color;
+        int qty;
+
+        public Part(string id, string name, string category, string color, int qty)
+        {
+            this.id = id;
+            this.name = name;
+            this.category = category;
+            this.color = color;
+            this.qty = qty;
+        }
+
+        public string Id => id;
+        public string Name => name;
+        public string Category => category;
+        public string Color => color;
+        public int Qty => qty;
+    }
+}
